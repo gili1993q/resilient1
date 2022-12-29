03fccdb135d62c2c6aac9175235f0ec0b6bfb1df
gili1993q <121624467+gili1993q@users.noreply.github.com>
2022-12-29 17:30:17 +0200

    Add files via upload

---
+++ ibmresilient/app-gili_calculator-1.0.0/gili_calculator-1.0.0/gili_calculator/components/funct_gili_multi.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+"""AppFunction implementation"""
+
+from resilient_circuits import AppFunctionComponent, app_function, FunctionResult
+from resilient_lib import IntegrationError, validate_fields
+
+PACKAGE_NAME = "gili_calculator"
+FN_NAME = "gili_multi"
+
+
+class FunctionComponent(AppFunctionComponent):
+    """Component that implements function 'gili_multi'"""
+
+    def __init__(self, opts):
+        super(FunctionComponent, self).__init__(opts, PACKAGE_NAME)
+
+    @app_function(FN_NAME)
+    def _app_function(self, fn_inputs):
+        """
+        Function: multiple 2 given numbers
+        Inputs:
+            -   fn_inputs.a
+            -   fn_inputs.b
+        """
+
+        yield self.status_message(f"Starting App Function: '{FN_NAME}'")
+
+        # Example validating app_configs
+        # validate_fields([
+        #     {"name": "api_key", "placeholder": "<your-api-key>"},
+        #     {"name": "base_url", "placeholder": "<api-base-url>"}],
+        #     self.app_configs)
+
+        # Example validating required fn_inputs
+        # validate_fields(["required_input_one", "required_input_two"], fn_inputs)
+
+        # Example accessing optional attribute in fn_inputs and initializing it to None if it does not exist (this is similar for app_configs)
+        # optional_input =  getattr(fn_inputs, "optional_input", None)
+
+        # Example getting access to self.get_fn_msg()
+        # fn_msg = self.get_fn_msg()
+        # self.LOG.info("fn_msg: %s", fn_msg)
+
+        # Example interacting with REST API
+        # res_client = self.rest_client()
+        # function_details = res_client.get(f"/functions/{FN_NAME}?handle_format=names")
+
+        # Example raising an exception
+        # raise IntegrationError("Example raising custom error")
+
+        ##############################################
+        # PUT YOUR FUNCTION IMPLEMENTATION CODE HERE #
+        ##############################################
+        yield self.status_message(f"fn_inputs.a = {fn_inputs.a}")
+        yield self.status_message(f"fn_inputs.b = {fn_inputs.b}")
+        
+        multiple = fn_inputs.a * fn_inputs.b
+        yield self.status_message(f"multiple = {multiple}")
+        # Call API implementation example:
+        # params = {
+        #     "api_key": self.app_configs.api_key,
+        #     "ip_address": fn_inputs.artifact_value
+        # }
+        #
+        # response = self.rc.execute(
+        #     method="get",
+        #     url=self.app_configs.api_base_url,
+        #     params=params
+        # )
+        #
+        # results = response.json()
+        #
+        # yield self.status_message(f"Endpoint reached successfully and returning results for App Function: '{FN_NAME}'")
+        #
+        # yield FunctionResult(results)
+        ##############################################
+
+        yield self.status_message(f"Finished running App Function: '{FN_NAME}'")
+
+        # Note this is only used for demo purposes! Put your own key/value pairs here that you want to access on the Platform
+        results = {
+            "content": multiple
+            }
+
+        yield FunctionResult(results)
+        #yield FunctionResult(multiple, success=True, reason=None)
+        #yield FunctionResult({}, success=False, reason="Bad call")
